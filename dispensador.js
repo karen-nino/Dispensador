7a4138bd073a9ec2fd92d2447ddc9129f90d399e
karen-nino <karen.nino23@gmail.com>
2019-01-31 21:53:10 -0600

    agrega los archivos html, css y js del proyecto  dispensador

---
+++ dispensador.js
@@ -0,0 +1,24 @@
+let cantidadMinimaCroquetas = 100;
+let cantidadCroquetasaServir = 50;
+let cantidadActualCroquetas = 80;
+
+function sirveComida() {
+    if (cantidadActualCroquetas < cantidadMinimaCroquetas) {
+    alert('Sirve Comida');
+    };
+
+}
+
+let horaActual = '13:00';
+let horaUno = '8:00';
+let horaDos = '13:00';
+let horaTres = '18:00';
+
+function horaComida() {
+    if (horaActual == horaDos) {
+    alert('Hora de alimentarlo');
+    };
+}
+
+
+
